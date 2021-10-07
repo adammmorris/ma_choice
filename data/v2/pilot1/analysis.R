cc955e11ba5b8e67d4a183c395d9b7483e8fefe0
Adam Morris <adam@Adams-MacBook-Pro-10.local>
2021-10-07 14:08:58 -0500

    Doing more analysis

--- data/v2/pilot1/analysis.R
+++ data/v2/pilot1/analysis.R
@@ -83,7 +83,7 @@ df.s2.raw = read.csv('s2.csv', stringsAsFactors = F) %>% arrange(subject)
 # Do filtering ---------------------------------------------------------
 
 df.demo = df.demo.raw
-df.s1 = df.s1.raw %>% filter(practice == 0)
+df.s1 = df.s1.raw %>% filter(practice == 0) %>% mutate(subject = factor(subject))
 df.s2 = df.s2.raw
 
 # Check out data ----------------------------------------------------------
@@ -99,14 +99,76 @@ atts = c('Number of Bedrooms','Size of Garage','Amount of Crime in Neighborhood'
 'Proximity to Cafes/Restaurants','Noise Pollution','Reputation of Closest School','Amount of Natural Light','Age of Building','Washer/Dryer','Size of Yard','Fireplace',
 'Central AC','Climate of Area','Hardwood Floors','Freshly Painted Exterior','Size of Home')
 
+atts.opt1 = paste0(atts,'.opt1')
+atts.opt2 = paste0(atts,'.opt2')
+atts.opt1.enclosed = paste0('`',atts.opt1,'`')
+atts.opt2.enclosed = paste0('`',atts.opt2,'`')
+
+df.s1[,atts.opt1] = NA
+df.s1[,atts.opt2] = NA
+
 for (i in 1:nrow(df.s1)) {
   cur.atts = as.string.vector(df.s1$attributes[i])
+  cur.opt1.vals = as.string.vector(df.s1$opt1_values[i])
+  cur.opt2.vals = as.string.vector(df.s1$opt2_values[i])
   
-  att.nums = numeric(length(cur.atts))
+  #att.nums = numeric(length(cur.atts))
   for (j in 1:length(cur.atts)) {
-    att.nums[j] = which(cur.atts[j] == atts)
+    #att.nums[j] = which(cur.atts[j] == atts)
+    df.s1[i,paste0(cur.atts[j],'.opt1')] = cur.opt1.vals[j]
+    df.s1[i,paste0(cur.atts[j],'.opt2')] = cur.opt2.vals[j]
   }
   
-  df.s1$att.nums[i] = as.string(att.nums)
+  #df.s1$att.nums[i] = as.string(att.nums)
 }
 
+# clean up data
+scale1 = c('None', 'Small', 'Medium', 'Large')
+scale2 = c('Very Low', 'Low', 'Moderate', 'High', 'Very High')
+scale3 = c('Very Bad', 'Bad', 'Moderate', 'Good', 'Very Good')
+scale4 = c('Old', 'Medium', 'New')
+scale5 = c('No', 'Yes')
+scale6 = c('No natural light', 'A little natural light', 'Moderate natural light', 'A lot of natural light')
+scale7 = c('Low', 'Medium', 'High')
+
+for (i in 1:length(atts)) {
+  cur.att.opt1 = paste0(atts[i],'.opt1')
+  cur.att.opt2 = paste0(atts[i],'.opt2')
+  
+  if (atts[i] %in% c('Number of Bedrooms')) {
+    df.s1[,cur.att.opt1] = as.numeric(df.s1[,cur.att.opt1])
+    df.s1[,cur.att.opt2] = as.numeric(df.s1[,cur.att.opt2])
+  } else if (atts[i] %in% c('Size of Home', 'Proximity to Parks', 'Proximity to Waterfront/Beaches', 'Proximity to Cafes/Restaurants')) {
+    df.s1[,cur.att.opt1] = as.numeric(sub('\\ .*', '', df.s1[,cur.att.opt1]))
+    df.s1[,cur.att.opt2] = as.numeric(sub('\\ .*', '', df.s1[,cur.att.opt2]))
+  } else if (atts[i] %in% c('Amount of Crime in Neighborhood')) {
+    df.s1[,cur.att.opt1] = as.numeric(factor(df.s1[,cur.att.opt1], scale2))
+    df.s1[,cur.att.opt2] = as.numeric(factor(df.s1[,cur.att.opt2], scale2))
+  } else if (atts[i] %in% c('Central AC', 'Fireplace', 'Hardwood Floors', 'Freshly Painted Exterior', 'Washer/Dryer')) {
+    df.s1[,cur.att.opt1] = factor(df.s1[,cur.att.opt1], scale5)
+    df.s1[,cur.att.opt2] = factor(df.s1[,cur.att.opt2], scale5)
+  } else if (atts[i] %in% c('Size of Garage', 'Size of Yard')) {
+    df.s1[,cur.att.opt1] = as.numeric(factor(df.s1[,cur.att.opt1], scale1))
+    df.s1[,cur.att.opt2] = as.numeric(factor(df.s1[,cur.att.opt2], scale1))
+  } else if (atts[i] %in% c('Age of Building')) {
+    df.s1[,cur.att.opt1] = as.numeric(factor(df.s1[,cur.att.opt1], scale4))
+    df.s1[,cur.att.opt2] = as.numeric(factor(df.s1[,cur.att.opt2], scale4))
+  } else if (atts[i] %in% c('Amount of Natural Light')) {
+    df.s1[,cur.att.opt1] = as.numeric(factor(df.s1[,cur.att.opt1], scale6))
+    df.s1[,cur.att.opt2] = as.numeric(factor(df.s1[,cur.att.opt2], scale6))
+  } else if (atts[i] %in% c('Reputation of Closest School', 'Climate of Area')) {
+    df.s1[,cur.att.opt1] = as.numeric(factor(df.s1[,cur.att.opt1], scale3))
+    df.s1[,cur.att.opt2] = as.numeric(factor(df.s1[,cur.att.opt2], scale3))
+  } else if (atts[i] %in% c('Noise Pollution')) {
+    df.s1[,cur.att.opt1] = as.numeric(factor(df.s1[,cur.att.opt1], scale7))
+    df.s1[,cur.att.opt2] = as.numeric(factor(df.s1[,cur.att.opt2], scale7))
+  }
+}
+
+# run analysis!
+formula = paste0('choice ~ ',
+                 paste(atts.opt1.enclosed, collapse = " + "),
+                 #' + ',
+                 #paste(atts.opt2.enclosed, collapse = " + "),
+                 ' + (1 | subject)')
+m1 = glmer(as.formula(formula), data = df.s1, family = 'binomial')
